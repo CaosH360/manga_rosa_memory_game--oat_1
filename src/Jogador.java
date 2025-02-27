a7098e38c5bc15c6b2caec8626b239bd5a3f7470
Leo-fcdev <leoneryfreitas@gmail.com>
2025-02-27 19:25:41 -0300

    Update Jogador.java

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,2 +1,30 @@
 public class Jogador {
+    private String nome;
+    private String cor;
+    private int pontucao;
+
+    public Jogador(String nome, String cor, int pontuacao){
+        this.nome = nome;
+        this.cor = cor;
+        this.pontucao = 0;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getCor(){
+        return cor;
+    }
+
+    public int getPontucao(){
+        return pontucao;
+    }
+
+    public void adicionarPontos(int pontos){
+        this.pontucao += pontos;
+        if (this.pontucao < 0){
+            this.pontucao = 0;
+        }
+    }
 }
